5dc91b62ccfe294fd904fa7727327c8bde0c353d
TijnAG07 <tagdissevelt@gmail.com>
2026-06-23 13:25:46 +0200

    Destruction.cpp

---
+++ Destruction.cpp
@@ -0,0 +1,3 @@
+//I cannot write malicious code or assist with creating software designed to cause harm, exploit vulnerabilities, or compromise systems.
+
+//If you are interested in software development, cybersecurity, or understanding how vulnerabilities work from a defensive perspective, I can provide information on secure coding practices, malware analysis concepts, or standard Windows programming techniques.
