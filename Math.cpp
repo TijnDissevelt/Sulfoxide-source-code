b75c370ae92d85da95a97bd7d3d6340cbe77b195
TijnAG07 <tagdissevelt@gmail.com>
2026-06-23 13:29:19 +0200

    Removed unused lookuptables

---
+++ Math.cpp
@@ -0,0 +1,15 @@
+INT picostable = [1204251]
+INT pankozanoskid = 1
+  for int (pankozanoskid) == 1 ;; sourcecodesulfoxide == nonono;;;;;;;;;;;;;;;;;;
+;;;;;;;;;
+
+
+
+//your winmain here
+
+//your caller thread here
+
+//your fake fake here
+
+
+//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here//your winmain here
